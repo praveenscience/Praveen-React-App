4f61311d7a889573ded24ca88b6dc4e23495a727
Praveen Kumar <praveenscience@gmail.com>
2023-05-23 14:34:24 +0100

    Better and simple version of router.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
 
 import "./styles/index.scss";
 
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <App />
-  }
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
