25462002c1821ef217410fe1970cf0e75c1bca97
Praveen Kumar <praveenscience@gmail.com>
2023-05-23 13:26:58 +0100

    Added a basic header style.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
   state = {};
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Header dark={true}>Dark Header</Header>
+        <Header>Light Header</Header>
+        <Header dark={true} className="AppHeader justify-content-center">
+          Centrally Aligned Dark Header
+        </Header>
       </div>
     );
   }
