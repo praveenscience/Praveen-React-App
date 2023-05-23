25055b9526e0664c961db5b57d6c9263620c35b3
Praveen Kumar <praveenscience@gmail.com>
2023-05-23 13:08:46 +0100

    Cleaned up from CRA.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
